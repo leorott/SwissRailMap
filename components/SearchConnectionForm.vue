a30d492dbc47e61e113bce430283869f742f0399
Leo Roettele <leo.roettele@liip.ch>
2023-01-16 22:43:11 +0100

    WIP: search connections

---
+++ components/SearchConnectionForm.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="mb-3">
+    <label for="from" class="form-label">Von</label>
+    <input v-model="from" type="text" class="form-control" id="from">
+  </div>
+  <div class="mb-3">
+    <label for="to" class="form-label">Nach</label>
+    <input v-model="to" type="text" class="form-control" id="to">
+  </div>
+  <button @click="$emit('search', { from, to })" class="btn btn-primary">Verbindung suchen</button>
+</template>
+<script setup>
+import { ref } from 'vue';
+const from = ref(null);
+const to = ref(null);
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,26 @@
 <template>
   <div>
-    <h1>TEST</h1>
+    <h1>Verbindung suchen</h1>
+    <search-connection-form @search="(e) => getConnections(e.from, e.to)"/>
+    <ul v-if="connectionsResponse">
+      <li v-for="connection in connectionsResponse.connections">
+        {{ connection }}
+      </li>
+    </ul>
   </div>
 </template>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const connectionsResponse = ref(null);
+const getConnections = async (from: string, to: string) => {
+  const res = await fetch(
+      `http://transport.opendata.ch/v1/connections?from=${from}&to=${to}`,
+      {
+        method: 'GET',
+      }
+  );
+  connectionsResponse.value = await res.json()
+}
+
+</script>
