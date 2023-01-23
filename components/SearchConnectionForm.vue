5f66ad6c54c142fb89b8b7a45bd95ad02c9fff47
Leo Roettele <leo.roettele@liip.ch>
2023-01-23 17:54:28 +0100

    search with date and time

--- components/SearchConnectionForm.vue
+++ components/SearchConnectionForm.vue
@@ -7,10 +7,33 @@
     <label for="to" class="form-label">Nach</label>
     <input v-model="to" type="text" class="form-control" id="to">
   </div>
-  <button @click="$emit('search', { from, to })" class="btn btn-primary">Verbindung suchen</button>
+  <div class="mb-3">
+    <label for="to" class="form-label">Nach</label>
+    <input v-model="dateTime" type="datetime-local" class="form-control" id="to">
+  </div>
+  <button @click="$emit('search', connectionForm)" class="btn btn-primary">Verbindung suchen</button>
 </template>
-<script setup>
-import { ref } from 'vue';
-const from = ref(null);
-const to = ref(null);
+<script setup lang="ts">
+import { Ref, ref } from 'vue';
+
+interface ConnectionForm {
+  from?: Ref<string>
+  to?: Ref<string>
+  dateTime: Ref<string>
+}
+
+const from = ref<string>('')
+const to = ref<string>('')
+const dateTime = ref<string>('');
+
+const connectionForm = computed<ConnectionForm>(() => {
+  return {
+    from,
+    to,
+    dateTime
+  }
+})
+
+
+
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Verbindung suchen</h1>
-    <search-connection-form @search="(e) => getConnections(e.from, e.to)"/>
+    <search-connection-form @search="(e) => getConnections(e.from.value, e.to.value, e.dateTime.value)"/>
     <ul v-if="connectionsResponse">
       <li v-for="connection in connectionsResponse.connections">
         {{ connection }}
@@ -13,14 +13,18 @@
 import { ref } from 'vue';
 
 const connectionsResponse = ref(null);
-const getConnections = async (from: string, to: string) => {
+const getConnections = async (from: string, to: string, dateTimeString: string) => {
+  const dateTime = new Date(dateTimeString);
+  const date = `${dateTime.getFullYear()}-${dateTime.getMonth() + 1}-${dateTime.getDate()}`;
+  const time = `${dateTime.getHours()}:${dateTime.getMinutes()}`
   const res = await fetch(
-      `http://transport.opendata.ch/v1/connections?from=${from}&to=${to}`,
+      `http://transport.opendata.ch/v1/connections?from=${from}&to=${to}&date=${date}&time=${time}`,
       {
         method: 'GET',
       }
   );
   connectionsResponse.value = await res.json()
 }
+
 
 </script>
